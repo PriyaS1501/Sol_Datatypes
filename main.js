33b3b612f19856a67c4be661d50046de7ff28176
PriyaS1501 <sawantpriyanka323@gmail.com>
2020-03-29 23:56:38 +0530

    data type

---
+++ main.js
@@ -0,0 +1,36 @@
+// Data types
+let _numValue = 10; // Declaration with assigning
+
+// Number
+let numValue; // Declaration
+    numValue=10; // assign
+
+    console.log(numValue);
+    console.log (typeof(numValue));
+
+ //boolean
+ 
+ let boolValue= true;
+ console.log(boolValue);
+ console.log (typeof (boolValue));
+
+ //string
+
+ let strValue= 'priya';
+ console.log(strValue);
+ console.log(typeof (strValue));
+
+ //undefined
+
+ let undefinedValue= undefined;
+ console.log(undefinedValue);
+ console.log(typeof (undefinedValue));
+
+ // Null
+
+ let nullValue= null;
+ console.log(nullValue);
+ console.log(typeof (nullValue));
+
+ let strValue1 ="Piu";
+ strValue1 = null;
